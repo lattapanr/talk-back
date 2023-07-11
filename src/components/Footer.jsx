c0e12573c7ab3284bd089f78b817a0b6a029d9e0
Noey <noey.rachata@gmail.com>
2023-07-11 15:47:28 +0700

    Add height and margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="h-12 w-full flex justify-end items-end">
+    <footer className="h-12 w-full flex justify-end items-end mb-4 lg:h-24 lg:mb-10">
       <p className="text-xs text-charcoal dark:text-off-white ">
         Made by{" "}
         <a
           href="https://github.com/lattapanr"
           target="_blank"
-          className="hover:underline"
+          className="italic hover:underline hover:text-park-green dark:hover:text-desert-gold"
         >
           Lattapan
         </a>
